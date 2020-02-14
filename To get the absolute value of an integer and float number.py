1384589c8312d864c13bc6363b778035ec1edfba
SyedMohammedAli77DS <58073063+SyedMohammedAli77DS@users.noreply.github.com>
2020-02-14 15:47:52 +0530

    Add files via upload

---
+++ To get the absolute value of an integer and float number.py
@@ -0,0 +1,15 @@
+# testing abs() for an integer and float
+
+int_num = -25
+
+float_num = -10.50
+
+print("The absolute value of an integer number is:", abs(int_num))
+print("The absolute value of a float number is:", abs(float_num))
+
+# testing abs() for a complex number
+
+complex_num = (3+10j)
+
+print("The magnitude of the complex number is:", abs(complex_num))
+
